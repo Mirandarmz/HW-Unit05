5947fd2f74c0a2e7170d66840aabd281912d9a4f
mirandarmz <mirandarmz.c@gmail.com>
2021-04-14 12:50:49 -0500

    Added comments to the code and the readme file

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@
 initStorage();
 initialize();
 
+//Function that loads the colors of the text areas according to the time of the day that is currently happening 
 function initialize(){
     var hourNow;
     var id;
-    for(i=9;i<=17;i++){
+    for(i=9;i<=17;i++){ //Loop that goes through each text area and evaluates which color it needs
         hourNow=moment().format('h');
-        id="#hour-"+i;
-        console.log(id)
-        if(hourNow>i){
+        id="#hour-"+i;//We concatenate an id to automatize the process 
+        if(hourNow>i){ //if the current time is above the hour text area we are visiting, add class past 
             $(id).addClass("past");
         }
         else{
-            if(hourNow==i){
+            if(hourNow==i){ //if the current time is equal to the hour text area we are visiting, add class present
                 $(id).addClass("present");
             }
-            else{
+            else{ //if the current time is below the hour text area we are visiting, add class future
                 $(id).addClass("future");
             }
         }
@@ -24,6 +24,7 @@ function initialize(){
     }
 }
 
+//We create an array of objects that have as property the id (to each container of each how) and text 
 var todos = [{ 
   id: "#hour-9",
   text: "",
@@ -53,11 +54,11 @@ var todos = [{
   text: "",
 }]; 
 
-//Rendering of initials
+//Rendering of todos written
 function renderTodos() {
-  for (var i = 0; i < todos.length; i++) { //loop generates and appends the li element to the highscore section for them to appear correctly 
+  for (var i = 0; i < todos.length; i++) { //Loop that sets the value of the text areas to the ones saved on the storage 
     var todo = todos[i];
-    $(todo.id).children(".description").val(todo.text);
+    $(todo.id).children(".description").val(todo.text);//Text areas have a class assigned as description, we access them through children 
     
  }
 }
@@ -74,12 +75,10 @@ function initStorage() {
 
 }
 
-//Event listener for the submit initials button to function 
+//Event listener for the save button to function 
 $(".saveBtn").click(function (event) {
-  $.trim($("#comment").val());
-  for(var i=0;i<todos.length;i++){
+  for(var i=0;i<todos.length;i++){ //For every element in the array, change the property text to the text that is currently written in the text area
     todoText=$(todos[i].id).children(".description").val();
-    console.log(todoText);
     todos[i].text=todoText;
 }
   localStorage.setItem("todos", JSON.stringify(todos));//Local storage
