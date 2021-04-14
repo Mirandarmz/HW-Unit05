2b81fca31c644983f6137ce4a081d56110706055
mirandarmz <mirandarmz.c@gmail.com>
2021-04-14 14:21:58 -0500

    Moved the click event to a separate function that is called with each click to optimize

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 //Two functions will be executed when the page loads: initalization of storage and initalization of 
-initStorage();
 initialize();
+initStorage();
 
 //Function that loads the colors of the text areas according to the time of the day that is currently happening 
 function initialize(){
     var hourNow;
     var id;
     for(i=9;i<=17;i++){ //Loop that goes through each text area and evaluates which color it needs
-        hourNow=moment().format('h');
+        hourNow=moment().format('HH');
         id="#hour-"+i;//We concatenate an id to automatize the process 
         if(hourNow>i){ //if the current time is above the hour text area we are visiting, add class past 
             $(id).addClass("past");
@@ -59,7 +59,6 @@ function renderTodos() {
   for (var i = 0; i < todos.length; i++) { //Loop that sets the value of the text areas to the ones saved on the storage 
     var todo = todos[i];
     $(todo.id).children(".description").val(todo.text);//Text areas have a class assigned as description, we access them through children 
-    
  }
 }
 
@@ -75,12 +74,42 @@ function initStorage() {
 
 }
 
-//Event listener for the save button to function 
-$(".saveBtn").click(function (event) {
-  for(var i=0;i<todos.length;i++){ //For every element in the array, change the property text to the text that is currently written in the text area
-    todoText=$(todos[i].id).children(".description").val();
-    todos[i].text=todoText;
-}
+function saveText(i){
+  var todoText=$(todos[i].id).children(".description").val();
+  todos[i].text=todoText;
+  console.log(todoText);
   localStorage.setItem("todos", JSON.stringify(todos));//Local storage
   renderTodos();
+}
+
+//Event listener for the save button to function 
+$("#button-9").click(function (event) {
+  saveText(0);
 });
+$("#button-10").click(function (event) {
+  saveText(1);
+});
+$("#button-11").click(function (event) {
+  saveText(2);
+});
+$("#button-12").click(function (event) {
+  saveText(3);
+});
+$("#button-13").click(function (event) {
+  saveText(4);
+});
+$("#button-14").click(function (event) {
+  saveText(5);
+});
+$("#button-15").click(function (event) {
+  saveText(6);
+});
+$("#button-16").click(function (event) {
+  saveText(7);
+});
+$("#button-17").click(function (event) {
+  saveText(8);
+});
+
+
+
